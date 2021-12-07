823dddc2a466122d0795a342ccc932de4f31933c
Timo <developer.timo@gmail.com>
2021-12-08 07:17:19 +0900

    [API 예외 처리] @ControllerAdvice > 패키지로 범위 지정

--- src/main/java/hello/exception/exhandler/advice/ExControllerAdvice.java
+++ src/main/java/hello/exception/exhandler/advice/ExControllerAdvice.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
-@RestControllerAdvice
+@RestControllerAdvice("hello.exception.exhandler")
 public class ExControllerAdvice {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
